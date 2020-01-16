651f4e8f26dbb9f5532bee15a76f9b6b67fe3ac3
root <1437804128@qq.com>
2020-01-16 10:30:17 +0800

    openssl update

--- coremail_python/coremail_command/configing/simple_userlogin.py
+++ coremail_python/coremail_command/configing/simple_userlogin.py
@@ -4,16 +4,20 @@ import os
 import re
 
 def simple_common_user_login(user_information):
-    mail_host = "192.168.173.209"
+    """
+    get user session id
+    :param user_information: user information
+    :return:
+    """
     sid_return = os.popen("/home/coremail/bin/userutil --call-api cmd=5\&user_at_domain={user_name}"
                           .format(user_name=user_information["user_name"])).readlines()
     #print sid_return[0]
     sid_split = re.split("chars\)", sid_return[0])
     sid = re.split(os.linesep, sid_split[1])
     print "Info: webmail user {user_name} simple point login".format(user_name=user_information["user_name"])
-    print "http://{mail_host}/coremail/main.jsp?sid={sid}".format(mail_host=user_information["host"], sid=sid[0])
+    print "http://{mail_host}/coremail/main.jsp?sid={sid}".format(mail_host=user_information["server_host"], sid=sid[0])
 
     print "----------------"
     print "Info: webadmin {user_name} simple point login".format(user_name=user_information["user_name"])
-    print "http://{mail_host}/webadmin/~{sid}/~/usr/index_usr.jsp".format(mail_host=mail_host, sid=sid[0])
+    print "http://{mail_host}/webadmin/~{sid}/~/usr/index_usr.jsp".format(mail_host=user_information["server_host"], sid=sid[0])
 
--- coremail_python/coremail_command/run-coremail-command.py
+++ coremail_python/coremail_command/run-coremail-command.py
@@ -5,7 +5,8 @@ from configing import simple_userlogin
 user_information = {
     "user_name": "admin",
     "domain": "leejay.vip",
-    "host": "192.168.173.209"
+    "server_host": "192.168.173.209"
 }
 simple_userlogin.simple_common_user_login(user_information)
+
 
